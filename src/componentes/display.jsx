44a08249276c63257e0c3bb1df99c9e59c35518f
Leonardo Silva <61755496+professorx-x@users.noreply.github.com>
2023-05-11 10:06:59 -0300

    Update display.jsx

--- src/componentes/display.jsx
+++ src/componentes/display.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import '../css/display.css';
 
-export default (props) =>
+const flecha = (props) =>
 {
     // return <div className="display"> <label id="pri_display" className="pri_display"> {props.str} </label> <label id="sec_display" className="sec_display"> </label> </div>;
     return <div className="display"> <input type="text" id="pri_display" className="pri_display" value={props.str} disabled/> <label id="sec_display" className="sec_display"> </label> </div>;
 }
+
+export default flecha;
